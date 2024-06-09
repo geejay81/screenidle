b403b6369961a45ba7a6db257a9c1f5cf0645f68
Greg Parr <greg.parr@blueyonder.co.uk>
2024-06-09 21:42:40 +0100

    changed height of dropdown and added border to image

--- components/client-apps/Combobox.tsx
+++ components/client-apps/Combobox.tsx
@@ -63,14 +63,15 @@ function ComboBox({selectedItem, dispatch, srcUrl}: ComboBoxProps) {
           </label>
           <div className="flex shadow-sm bg-white gap-0.5">
             <input
+              autoComplete="off"
               id="search-field"
               placeholder="Search movies"
-              className="w-full px-1.5 py-4 border border-black focus:border-2 text-black"
+              className="w-full px-1.5 py-4 border-yellow focus:border-2 text-black mr-0 border-r-0"
               {...getInputProps()}
             />
             <button
               aria-label="clear selection"
-              className="p-4 border border-black focus:border-2 text-black"
+              className="p-4 border-yellow focus:border-2 text-black ml-0 border-l-0"
               type="button"
               onClick={() => dispatch({ type: 'SET_SELECTED_ITEM', payload: '' })}
             >
@@ -79,17 +80,19 @@ function ComboBox({selectedItem, dispatch, srcUrl}: ComboBoxProps) {
           </div>
         </div>
         <ul
-          className={`absolute w-72 bg-white mt-1 shadow-md max-h-80 overflow-scroll p-0 z-10 ${
-            !(isOpen && items.length) && 'hidden'
+          className={`absolute max-w-30 bg-white mt-1 shadow-md max-h-40 overflow-scroll p-0 z-10 ${
+            !(isOpen && items.length) ? 'hidden' : ''
           }`}
           {...getMenuProps()}
         >
           {isOpen &&
-            items.map((item: any, index) => (
-              <li className={
-                `py-2 px-3 shadow-sm text-black flex flex-col${highlightedIndex === index ? ' bg-slate-200' : ''}`}
+            items.map((item, index) => (
+              <li
+                className={`py-2 px-3 shadow-sm text-black flex flex-col ${
+                  highlightedIndex === index ? 'bg-slate-200' : ''
+                }`}
                 key={index}
-                {...getItemProps({item, index})}
+                {...getItemProps({ item, index })}
               >
                 <span>{item}</span>
               </li>
