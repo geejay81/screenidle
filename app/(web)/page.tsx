46c3871dada764d09681dee1478e1a9ca1a0bdb2
Greg Parr <greg.parr@blueyonder.co.uk>
2024-07-23 22:34:05 +0100

    updated homepage to include CTA to taglines game, and updated menu to be full screen on mobile

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -1,5 +1,6 @@
 import { headings, logo } from "@/ui/fonts";
 import Link from "next/link";
+import { FaQuoteLeft } from "react-icons/fa";
 import { FaClapperboard, FaPlay } from "react-icons/fa6";
 
 export default async function Home() {
@@ -11,8 +12,17 @@ export default async function Home() {
           <FaClapperboard className="inline" />
           <span>ScreenIdle</span>
         </h1>
-        <Link href="/posters" className={`home-cta ${headings.className}`}>
-          <FaPlay className="inline" />Play Today&apos;s Movie Poster Game</Link>
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <Link href="/posters" className={`home-cta ${headings.className} m-4`}>
+            <FaPlay className="inline" />
+            <span>Play Today&apos;s Movie Poster Game</span>
+          </Link>
+          <Link href="/posters" className={`home-cta ${headings.className} m-4`}>
+            <FaQuoteLeft className="inline" />
+            <span>Play Today&apos;s Movie Tagline Game</span>
+          </Link>
+        </div>
+        
       </div>
     </main>
   );
