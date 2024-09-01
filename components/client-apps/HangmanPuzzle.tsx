0ada553a469592eb42d78174afa98a89848917d8
Greg Parr <greg.parr@blueyonder.co.uk>
2024-09-01 18:20:08 +0100

    Created common parent component ResultsMode in hangman game

--- components/client-apps/HangmanPuzzle.tsx
+++ components/client-apps/HangmanPuzzle.tsx
@@ -17,6 +17,15 @@ interface ResultMessageProps {
     children: React.ReactNode;
 }
 
+interface ResultModeProps {
+    children: React.ReactNode
+}
+
+interface PosterImageProps {
+    imageUrl: string,
+    alt: string
+}
+
 export default function HangmanPuzzle({movie}: HangmanPuzzleProps) {
     const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
     const [wrongGuesses, setWrongGuesses] = useState<string[]>([]);
@@ -63,38 +72,37 @@ export default function HangmanPuzzle({movie}: HangmanPuzzleProps) {
     )
 
     const LostMode = () => (
-        <div className="grid md:grid-cols-2 gap-4 md:gap-8 mt-0">
-            <div className="space-y-4 mt-0">
-                <PosterImage imageUrl={movie.poster} alt={`${movie.title} poster`} />
-            </div>
-            <div className="space-y-4 mt-0">
-                <LifeBar lives={initialLives} livesUsed={wrongGuesses.length} color="danger" />
-                <ResultMessage heading="Game Over!" bgColor="bg-screenidle-danger">
-                    <p>Bad luck. The answer to this puzzle was <span className="font-bold">{movie.title}</span>.</p>
-                </ResultMessage>
-            </div>
-            <div>
-                <p>Poster images provided by <a href="https://www.themoviedb.org" className="font-semibold">TMDB</a>.</p>
-            </div>
-        </div>
+        <ResultMode>
+            <ResultMessage heading="Game Over!" bgColor="bg-screenidle-danger">
+                <p>Bad luck. The answer to this puzzle was <span className="font-bold">{movie.title}</span>.</p>
+            </ResultMessage>
+        </ResultMode>
     )
 
     const WonMode = () => (
-        <div className="grid md:grid-cols-2 gap-4 md:gap-8 mt-0">
-            <div className="space-y-4 mt-0">
-                <PosterImage imageUrl={movie.poster} alt={`${movie.title} poster`} />
-            </div>
-            <div className="space-y-4 mt-0">
-                <LifeBar lives={initialLives} livesUsed={wrongGuesses.length} color="danger" />
-                <ResultMessage heading="Winner!" bgColor="bg-screenidle-success">
-                    <p>Congatulations! You knew that the answer was <span className="font-bold">{movie.title}</span>.</p>
-                </ResultMessage>
-            </div>
-            <div>
-                <p>Poster images provided by <a href="https://www.themoviedb.org" className="font-semibold">TMDB</a>.</p>
-            </div>
-        </div>
+        <ResultMode>
+            <ResultMessage heading="Winner!" bgColor="bg-screenidle-success">
+                <p>Congatulations! You knew that the answer was <span className="font-bold">{movie.title}</span>.</p>
+            </ResultMessage>
+        </ResultMode>
     )
+
+    const ResultMode = ({children}: ResultModeProps) => {
+        return (
+            <div className="grid md:grid-cols-2 gap-4 md:gap-8 mt-0">
+                <div className="space-y-4 mt-0">
+                    <PosterImage imageUrl={movie.poster} alt={`${movie.title} poster`} />
+                </div>
+                <div className="space-y-4 mt-0">
+                    <LifeBar lives={initialLives} livesUsed={wrongGuesses.length} color="danger" />
+                    {children}
+                </div>
+                <div>
+                    <p>Poster images provided by <a href="https://www.themoviedb.org" className="font-semibold">TMDB</a>.</p>
+                </div>
+            </div>
+        )
+    }
 
     switch (gameMode) {
         case 'won': return <WonMode />
@@ -110,11 +118,6 @@ const ResultMessage = ({heading, bgColor, children} : ResultMessageProps) => {
             {children}
         </div>
     )
-}
-
-interface PosterImageProps {
-    imageUrl: string,
-    alt: string
 }
 
 const PosterImage = ({imageUrl, alt}: PosterImageProps) => {
