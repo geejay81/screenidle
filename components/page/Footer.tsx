70276970d9df059ce6a64de450f70cff7f2205a5
Greg Parr <greg.parr@blueyonder.co.uk>
2024-09-22 21:36:04 +0100

    removed old cookie icon from footer

--- components/page/Footer.tsx
+++ components/page/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { FaCookie, FaXTwitter } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
 
@@ -20,9 +20,6 @@ export default function Footer() {
                         {link.icon}<span className="sr-only">Visit ScreenIdle on {link.title}</span>
                     </Link>
                 ))}
-                {/* <a href="" onClick={"javascript:adthrive.cmd.push(adthrive.showPrivacyPreferences);"} id="open_preferences_center" className="p-4" title="Manage cookies">
-                    <FaCookie /><span className="sr-only">Manage cookies</span>
-                </a> */}
                 <a href="/privacy" className="p-4" title="Privacy">
                     Privacy
                 </a>
