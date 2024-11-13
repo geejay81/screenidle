c26eb2209ba5756dd49f3bb8866451f763ddfa25
Greg Parr <greg.parr@blueyonder.co.uk>
2024-11-13 22:55:39 +0000

    attempted build of static site

--- app/(api)/api/caching/route.ts
+++ app/(api)/api/caching/route.ts
@@ -1,38 +1,38 @@
 import { revalidatePath } from 'next/cache'
 import { NextRequest, NextResponse } from 'next/server'
 
-export const dynamic = "force-dynamic"
+//export const dynamic = "force-dynamic"
  
-export async function GET(request: NextRequest) {
+// export async function GET(request: NextRequest) {
 
-    try {
+//     try {
 
-        if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-            return NextResponse.json({ error: 'Unauthorized' }, {
-                status: 401 })
-        }
+//         if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+//             return NextResponse.json({ error: 'Unauthorized' }, {
+//                 status: 401 })
+//         }
         
-        revalidatePath('/(api)/api/movies', 'page');
-        revalidatePath('/(web)/posters', 'page');
-        revalidatePath('/(web)/posters/history', 'page');
-        revalidatePath('/(web)/taglines', 'page');
-        revalidatePath('/(web)/taglines/history', 'page');
-        revalidatePath('/(web)/blank-buster', 'page');
-        revalidatePath('/(web)/blank-buster/history', 'page');
-        return NextResponse.json({ revalidated: true, now: Date.now() });
+//         revalidatePath('/(api)/api/movies', 'page');
+//         revalidatePath('/(web)/posters', 'page');
+//         revalidatePath('/(web)/posters/history', 'page');
+//         revalidatePath('/(web)/taglines', 'page');
+//         revalidatePath('/(web)/taglines/history', 'page');
+//         revalidatePath('/(web)/blank-buster', 'page');
+//         revalidatePath('/(web)/blank-buster/history', 'page');
+//         return NextResponse.json({ revalidated: true, now: Date.now() });
 
-    } catch (err) {
+//     } catch (err) {
 
-        let errorMessage = 'error revalidating data';
+//         let errorMessage = 'error revalidating data';
         
-        if (err instanceof Error) {
-            errorMessage = `error revalidating data: ${err.message}`
-        }
+//         if (err instanceof Error) {
+//             errorMessage = `error revalidating data: ${err.message}`
+//         }
             
-        return NextResponse.json({
-            revalidated: false,
-            now: Date.now(),
-            message: errorMessage,
-        });
-    }
-}
+//         return NextResponse.json({
+//             revalidated: false,
+//             now: Date.now(),
+//             message: errorMessage,
+//         });
+//     }
+// }
--- app/(api)/api/movies/route.ts
+++ app/(api)/api/movies/route.ts
@@ -1,17 +1,17 @@
 import { getAllMovies } from '@/data/movies'
 import { NextResponse } from 'next/server'
  
-export async function GET(request: Request) {
+// export async function GET(request: Request) {
 
-    const movies = await getAllMovies();
+//     const movies = await getAllMovies();
 
-    // TODO: handle errors here.
+//     // TODO: handle errors here.
 
-    const result = movies.map((movie: any) => (
-        {
-            id: `${movie._id}`,
-            value: `${movie.title} (${movie.year})`
-        }
-    ));
-    return NextResponse.json({ movies: result })
-}
+//     const result = movies.map((movie: any) => (
+//         {
+//             id: `${movie._id}`,
+//             value: `${movie.title} (${movie.year})`
+//         }
+//     ));
+//     return NextResponse.json({ movies: result })
+// }
