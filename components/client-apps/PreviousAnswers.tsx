9f4d228611c71adefe56db1d825a89ffe84a2d97
Greg Parr <greg.parr@blueyonder.co.uk>
2025-10-29 22:49:48 +0000

    moved previous answer styling to stylesheet rather than inline

--- components/client-apps/PreviousAnswers.tsx
+++ components/client-apps/PreviousAnswers.tsx
@@ -24,15 +24,14 @@ export default function PreviousAnswers({ guesses }: PreviousAnswersProps) {
     }
     return <>
         {guesses.length > 0 && (
-            <div className="mt-2">
-                <h2 className={`py-2 font-bold text-lg ${headings.className}`}>
+            <div className="previous-answers">
+                <h2 className={`${headings.className}`}>
                     Previous guesses
                 </h2>
-                <ol className="space-y-2">
+                <ol className="previous-answers-list">
                     {guesses.slice(-6).map((guess: Guess, index: number) => (
                         <li
                             key={index}
-                            className="flex items-center justify-between p-3 bg-[color:rgb(200,100,255,0.125)] border-0 rounded-lg shadow-sm"
                         >
                             <span className="font-medium">{guess.answer && guess.answer.length > 0 ? guess.answer : "(Skipped)"}{guess.result === 'skipped' || <i className="sr-only">{guess.result}</i>}</span>
                             <span>{getResultIcon(guess.result)}</span>
